747f9cd33b58d15c805945fb7794cd98198d5314
YUHER\yuher <yuherlim123@gmail.com>
2024-11-26 23:40:25 +0800

    import google_fonts in assets

---
+++ lib/core/shared/text_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:timetailor/core/theme/customTheme.dart';
+
+TextStyle headingStyle({TextStyle? style}) {
+  final baseStyle = GoogleFonts.kanit(
+    textStyle: primaryTheme.textTheme.headlineMedium,
+  );
+  return baseStyle.merge(style);
+}
