5c5b1531941f1f004a6d8b8e95401bc52b1168de
YUHER\yuher <yuherlim123@gmail.com>
2024-11-27 17:54:34 +0800

    equal width of datebox

--- lib/screens/task_management/calendar_header.dart
+++ lib/screens/task_management/calendar_header.dart
@@ -17,16 +17,30 @@ class CalendarHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Total horizontal padding of the parent container
+    const double horizontalPadding = 16; // 8 (left) + 8 (right)
+    const double spacingBetweenContainers = 8; // Space between each container
+    final int numberOfDates = weekDates.length;
+
+    // Calculate available width for the containers
+    final double availableWidth = MediaQuery.of(context).size.width -
+        horizontalPadding -
+        (spacingBetweenContainers * (numberOfDates - 1));
+
+    // Width for each container
+    final double containerWidth = availableWidth / numberOfDates;
+
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: weekDates.map((date) {
           return GestureDetector(
             onTap: () {
               onDateSelected(date);
             },
             child: Container(
+              width: containerWidth,
               padding: const EdgeInsets.all(8),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
@@ -36,10 +50,10 @@ class CalendarHeader extends StatelessWidget {
               ),
               child: Column(
                 children: [
-                  DayBoxText(
+                  DateBoxText(
                     DateFormat('EEE').format(date), // Day of the week
                   ),
-                  DayBoxText(
+                  DateBoxText(
                     date.day.toString(), // Date
                   ),
                 ],
