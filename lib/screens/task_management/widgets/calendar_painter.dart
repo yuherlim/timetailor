7567522fea5639b5abe0d07f9bcb6bf857919ade
YUHER\yuher <yuherlim123@gmail.com>
2024-12-06 15:57:45 +0800

    removed calculateMaxTextWidth

--- lib/screens/task_management/widgets/calendar_painter.dart
+++ lib/screens/task_management/widgets/calendar_painter.dart
@@ -124,24 +124,4 @@ class CalendarPainter extends CustomPainter {
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return false; // No need to repaint unless the times change
   }
-
-  double calculateMaxTextWidth(List<String> timePeriods, TextStyle textStyle) {
-    double maxWidth = 0.0;
-
-    final textPainter = TextPainter(
-      textAlign: TextAlign.left,
-      textDirection: TextDirection.ltr,
-    );
-
-    for (final time in timePeriods) {
-      textPainter.text = TextSpan(
-        text: time,
-        style: textStyle,
-      );
-      textPainter.layout(); // Calculate the dimensions of the text
-      maxWidth = maxWidth < textPainter.width ? textPainter.width : maxWidth;
-    }
-
-    return maxWidth;
-  }
 }
