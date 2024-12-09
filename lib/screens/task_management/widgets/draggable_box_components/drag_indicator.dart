e5496feb83841ad2a30accd87e5e9c3d0f922d9b
YUHER\yuher <yuherlim123@gmail.com>
2024-12-09 19:12:24 +0800

    removed unused imports

--- lib/screens/task_management/widgets/draggable_box_components/drag_indicator.dart
+++ lib/screens/task_management/widgets/draggable_box_components/drag_indicator.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:timetailor/core/theme/custom_theme.dart';
 import 'package:timetailor/domain/task_management/providers/calendar_local_state_provider.dart';
 import 'package:timetailor/domain/task_management/providers/calendar_read_only_provider.dart';
