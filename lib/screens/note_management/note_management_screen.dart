6aa70569eaa26b1ffc5fa6a664da88c74152a4b5
YUHER\yuher <yuherlim123@gmail.com>
2024-12-21 03:25:41 +0800

    initial screen setup

--- lib/screens/note_management/note_management_screen.dart
+++ lib/screens/note_management/note_management_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timetailor/core/shared/widgets/styled_text.dart';
+import 'package:timetailor/core/theme/custom_theme.dart';
 
 class NoteManagementScreen extends ConsumerStatefulWidget {
   const NoteManagementScreen({super.key});
@@ -13,17 +14,46 @@ class NoteManagementScreen extends ConsumerStatefulWidget {
 class _NoteManagementScreenState extends ConsumerState<NoteManagementScreen> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
       appBar: AppBar(
+        backgroundColor: AppColors.appBarColor,
         title: const AppBarText("Notes"),
         centerTitle: true,
       ),
-      body: const Placeholder(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Expanded(
+            child: Center(
+              child: TitleTextInHistory(
+                "There are currently no notes.",
+              ),
+            ),
+          )
+
+          // completedTasks.isEmpty
+          //     ? const Expanded(
+          //         child: Center(
+          //           child: TitleTextInHistory(
+          //             "There are currently no notes.",
+          //           ),
+          //         ),
+          //       )
+          //     : Expanded(
+          //         child: ListView.builder(
+          //           itemCount: completedTasks.length,
+          //           itemBuilder: (context, index) {
+          //             final task = completedTasks[index];
+          //             return CompletedTaskListItem(task: task);
+          //           },
+          //         ),
+          //       ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/user_management/account_management_screen.dart
+++ lib/screens/user_management/account_management_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timetailor/core/shared/widgets/styled_text.dart';
+import 'package:timetailor/core/theme/custom_theme.dart';
 
 class AccountManagementScreen extends StatelessWidget {
   const AccountManagementScreen({super.key});
@@ -8,6 +9,7 @@ class AccountManagementScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.appBarColor,
         title: const AppBarText("Account Management"),
         centerTitle: true,
       ),
